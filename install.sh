41f96fd21608e600efd08127696a44b11e99d525
marconimp <marconimp@yahoo.com.br>
2017-05-18 22:55:47 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+sudo apt-get update && apt-get upgrade -y && apt-get install htop -y
+sudo apt-get install lsb-release nscd curl php5 php5-mysql php5-cli php5-curl unzip curl libcurl3 libcurl3-dev php5-curl -y
+sudo apt-get update && sudo apt-get install vlc vlc-plugin-* -y && sudo apt-get install vlc browser-plugin-vlc -y
 sudo apt-get update
 sudo apt-get install psmisc
-sudo apt-get upgrade
+sudo apt-get update
 sudo apt-get install unzip
+sudo apt-get update
 sudo apt-get install unrar
+sudo apt-get upgrade
 wget http://telecineplay.esy.es/ezserver.tgz
 tar -xzvf ezserver.tgz
 rm ezserver.tgz
